4ff14debf7bb7b5e76131a452446c4f8830f8e1a
郑仪旺 <zhengyw55@gmail.com>
2015-03-27 07:39:36 +0800

    S1

---
+++ Asynchronous JavaScript/S1/index.js
@@ -0,0 +1,38 @@
+﻿var adders = [];
+
+window.onload = function () {
+    var IDs = ['A', 'B', 'C', 'D', 'E'];
+
+    for (var i = 0; i < 5; i++) {
+        adders[i] = document.getElementById(IDs[i]);
+        adders[i].setAttribute('onclick', 'getRandomNumFromBackEnd(this, ' + i + ')');
+    }
+}
+
+function getRandomNumFromBackEnd(li, i) {
+    unread = adders[i].getElementsByTagName('strong')[0];
+    unread.style.opacity = 1.0;
+    unread.innerText = '。。。';
+    disableClick(i);
+
+    var req = new XMLHttpRequest();
+    req.open('GET', '../getRandomNum', true);
+    req.send();
+    req.onreadystatechange = function (li) {
+        if (req.readyState == 4 && req.status == 200) {
+            console.log(req.response);
+            console.log(adders[i]);
+            unread.innerText = req.response;
+        }
+    }
+}
+
+// 把除了下标为i的li的click函数去除，颜色变为灰色
+function disableClick(senderID) {
+    console.log(senderID);
+    for (var i = 0; i < 5; i++) {
+        if (i != senderID) {
+            adders[i].setAttribute('')
+        }
+    }
+}
